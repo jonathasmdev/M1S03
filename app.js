578d17e01936ef554ed9b65740e7f8c38416e34f
Jonathas <jondejotam@gmail.com>
2022-12-17 12:42:49 -0300

    add 2

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ const procuraMinMax = (arr) => {
     }
 }
 
+// No documento js faça uma arrow function para retornar uma mensagem(string) de "Olá, Mundo!".
+// Armazene esta arrow function em uma constante(const) de nome mensagemOla.
+// Chame a função mensagemOla e exiba o retorno desta função no console.
+// Ajuste a função mensagemOla para que receba um parâmetro nome e retorne a mensagem "Olá, [nome]!".
 
+const mensagemOla = (nome) => `Olá, ${nome}!`;
